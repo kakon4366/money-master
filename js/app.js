90046a49d4b93b031edb8bcc235b48df572fdb4b
kakon4366 <kakonbarman4366@gmail.com>
2022-02-17 11:16:13 +0600

    Expense Total and Balance Updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
+//Total Expenses Amount func
+function getExpenseAmount(){
+    const foodInput = document.getElementById('food-total');
+    const foodAmount = parseFloat(foodInput.value);
+    const rentInput = document.getElementById('rent-total');
+    const rentAmount = parseFloat(rentInput.value);
+    const othersInput = document.getElementById('others-total');
+    const othersAmount = parseFloat(othersInput.value);
+
+   const expenseTotalAmount = foodAmount + rentAmount + othersAmount;
+   return expenseTotalAmount;
+}
+
+//total income amount func
+function getTotalIncome(){
+    //total income amount
+    const incomeInput = document.getElementById('income-total');
+    const incomeAmount = parseFloat(incomeInput.value);
+    return incomeAmount;
+}
 
 
 //calculate handler
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    console.log('clicked');
+    //total expense amount
+    const expenseAmount = getExpenseAmount();
+    //display total expense amount
+    const totalExpenseText = document.getElementById('total-expense');
+    totalExpenseText.innerText = expenseAmount;
+    //update balance
+    const totalBalanceText = document.getElementById('total-balance');
+    const totalIncome = getTotalIncome();
+    const totalBalance = totalIncome - expenseAmount;
+    totalBalanceText.innerText = totalBalance;
 })
